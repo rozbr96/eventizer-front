01ee9dde1bd7474e0a1f10a8d5e8e7da0704f8e5
Rosemilson <rozbr96@gmail.com>
2026-08-23 19:22:53 -0300

    feat: api event retrieval

--- lib/api/events.ts
+++ lib/api/events.ts
@@ -12,12 +12,37 @@ export interface Event {
   price_in_cents: number
   status: string
   organizer_id: number
-  metadata: any
+  metadata: EventMetadata
+}
+
+export interface EventMetadata {
+  id: number
+  adult: boolean
+  title: string
+  video: boolean
+  overview: string
+  softcore: boolean
+  genre_ids: Array<number>
+  popularity: number
+  vote_count: number
+  poster_path: string
+  release_date: string
+  vote_average: number
+  backdrop_path: string
+  original_title: string
+  original_language: string
 }
 
 export type PaginatedEvents = PaginatedResult<Event>
 
 export default class extends APIEndpoint {
+  get(event_id: number): Promise<Event> {
+    return new Promise((resolve) => {
+      this.doRequest({ endpoint: `/events/${event_id}`, method: 'GET' })
+        .then((response) => { resolve(response.json()) })
+    })
+  }
+
   list(): Promise<PaginatedEvents> {
     return new Promise((resolve) => {
       this.doRequest({ endpoint: '/events', method: 'GET' })
